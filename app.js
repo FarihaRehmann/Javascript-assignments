d64b1d7689f5a2178ce295fdf75f5f66a1ece750
FarihaRehmann <fariharehman27>
2026-07-20 22:59:40 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// question 1
+alert("Greetings")
+// question 1 end
+
+// question2
+alert("Error! Please enter a valid password.")
+// question 2 end
+
+// question 3
+alert("Welcome to JS land...\n Happy coding!")
+// question 3 end
+
+// question 4
+alert("Welcome to JS land...")
+alert("Happy coding! \n Prevent this page from creating additional dialouges.")
+// question 4 end
+
+// question 5 
+// question 5 end
+
+// question 6
+alert("My Name is Fariha!")
+// question 6 end
+
+// question 7
+// question 7 end
---
+++ chapter 2/app.js
@@ -0,0 +1,29 @@
+// question 1
+var username;
+// question 2 end
+
+// question 2 
+var myName = "Fariha Rehman"; 
+// question 2 end
+
+// question 3
+var message;
+message = "Hello World";
+alert(message);
+
+// quesion 4
+var studentName = "Jone Doe";
+var studentAge = "15 years old";
+var studentCourse = "Certified Mobile Application Development";
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+// question 4 end
+
+// question 5 
+var alertMessage = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(alertMessage);
+// question 5 end
+
+
